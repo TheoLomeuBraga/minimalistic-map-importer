20473ebc77448c7a4d90cc07d26dfaca82f0e2ec
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2024-11-02 22:24:49 -0300

    a

--- include/mmapi/mmapi.h
+++ include/mmapi/mmapi.h
@@ -1,1 +1,19 @@
 #pragma once
+#include <string>
+#include "glm/glm.hpp"
+
+#ifndef MAP_SCALE
+#define MAP_SCALE 0.03125
+#endif
+
+class MAP_Plane
+{
+public:
+    MAP_Plane() {}
+    glm::vec3 position,normal;
+    MAP_Plane(glm::vec3 position , glm::vec3 normal) {
+        this->position = position;
+        this->normal = normal;
+    }
+};
+
