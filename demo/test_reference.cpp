af0cb3e0d0fbb48c6c48fcd0cc6ea94814e8e571
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2024-11-05 16:19:13 -0300

    a

--- demo/test_reference.cpp
+++ demo/test_reference.cpp
@@ -8,29 +8,23 @@
 
 int main()
 {
-    std::cout << "Iniciando conversão para obj...\n";
 
     MAPFile map;
     Entity *entities = NULL;
 
-    // Carrega o arquivo .map
     if (!map.Load("map.map", &entities))
     {
-        std::cout << "Erro ao carregar o arquivo map.map\n";
         return 0;
     }
 
-    // Abre o arquivo .obj para escrita
     std::ofstream objFile("obj.obj");
     if (!objFile.is_open())
     {
-        std::cerr << "Não foi possível abrir o arquivo obj.obj para escrita.\n";
         return 0;
     }
 
-    int vertexIndex = 1; // Para rastrear o índice do vértice para as faces
+    int vertexIndex = 1; 
 
-    // Itera pelas entidades
     int eid = 0;
     std::string last_texture;
 
@@ -43,12 +37,9 @@ int main()
     {
 
         e->CalculateEntityCenter();
-
-        // Itera pelos polígonos da entidade
         Poly *po = e->GetPolys();
         while (po != NULL)
         {
-            // Escreve a textura como comentário no arquivo OBJ
             objFile << "# Texture: " << po->TextureID << "\n";
 
             if (last_texture != po->TextureID)
@@ -58,29 +49,23 @@ int main()
                 last_texture = po->TextureID;
             }
 
-            // Converte o polígono em uma lista de triângulos
             for (const Triangle &t : po->convert_to_triangles())
             {
 
-                // Para cada vértice do triângulo
                 for (int i = 0; i < 3; ++i)
                 {
                     const Vertex &v = t.vertex[i];
 
-                    // Escreve a posição do vértice
                     objFile << "v " << v.p.x << " " << v.p.y << " " << v.p.z << "\n";
 
-                    // Escreve as coordenadas de textura do vértice
                     objFile << "vt " << v.tex[0] << " " << v.tex[1] << "\n";
                 }
 
-                // Escreve a face usando os índices dos três vértices do triângulo
                 objFile << "f "
                         << vertexIndex << "/" << vertexIndex << " "
                         << (vertexIndex + 1) << "/" << (vertexIndex + 1) << " "
                         << (vertexIndex + 2) << "/" << (vertexIndex + 2) << "\n";
 
-                // Atualiza o índice do próximo vértice
                 vertexIndex += 3;
             }
 
@@ -90,9 +75,6 @@ int main()
         e = e->GetNext();
     }
 
-    // Fecha o arquivo OBJ
     objFile.close();
-
-    std::cout << "Conversão para obj concluída.\n";
     return 0;
 }
